9203a3067136e0f7327e0694798ad3b0a104cfc1
AnjoomNur <anjoom.nur@northsouth.edu>
2021-11-23 23:49:54 +0600

    Login and registration page design

--- railapp/urls.py
+++ railapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('reg/', views.reg, name="reg"),
+    path('log/', views.log, name="log"),
 ]
--- railapp/views.py
+++ railapp/views.py
@@ -3,3 +3,7 @@ from .models import *
 # Create your views here.
 def home(request):
     return render(request, 'railapp/home.html')
+def reg(request):
+    return render(request, 'railapp/registration.html')
+def log(request):
+    return render(request, 'railapp/login.html')
