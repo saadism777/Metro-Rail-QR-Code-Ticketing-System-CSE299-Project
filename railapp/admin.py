eea1dbc5617e3e65a4f7b33b16d9b6f6c4ffab0a
AnjoomNur <anjoom.nur@northsouth.edu>
2021-11-25 18:23:00 +0600

    Database Model Created for RegistrationPage

--- railapp/admin.py
+++ railapp/admin.py
@@ -1,3 +1,8 @@
+
 from django.contrib import admin
+from railapp.models import User
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display=('id','firstname','lastname','username', 'email','phone','gender','password')
+admin.site.register(User,UserAdmin)
