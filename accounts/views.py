5bc294efc81be8f19b0047db47a70039855aa301
Farhan Hasin Saad <farhan.hasin09@northsouth.edu>
2021-11-29 02:16:06 +0600

    Implemented General User Login, Logout

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render,redirect
-from django.contrib.auth import login
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
-
+from django.contrib import messages
 
 from django.views.generic import CreateView
 
@@ -15,14 +15,43 @@ def SignUp(request):
 	return render(request,'register.html')
 
 
-
-def SignIn(request):
-	return render(request,'signin.html')
-
 def log(request):
-    return render(request, 'login.html')
+    """
+    This method is used to view the login page.
+    :param request: it's a HttpResponse from user.
+    :type request: HttpResponse.
+    :return: this method returns a search page which is a HTML page.
+    :rtype: HttpResponse.
+    """
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+          if request.method == 'POST':
+              username =request.POST.get('guser_name')
+              password =request.POST.get('guser_password')
+             
+              user= authenticate(request, username=username, password=password)
 
+              if user is not None:
+                  login(request, user)
+                  return redirect('home')
+              else:
+                  messages.info(request, 'Username or Password is incorrect')
+            
 
+    context= {}
+    return render(request, 'login.html', context)
+
+def log_out(request):
+    """
+    This method is used to logout the user and redirect them to the login page.
+    :param request: it's a HttpResponse from user.
+    :type request: HttpResponse.
+    :return: this method returns a search page which is a HTML page.
+    :rtype: HttpResponse.
+    """
+    logout(request)
+    return redirect('log')
 
 class GeneralUserSignUpView(CreateView):
     model = User
