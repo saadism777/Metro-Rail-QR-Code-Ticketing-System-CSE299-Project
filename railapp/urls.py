dc5b2865dbb549b0ba2ae0e5a48290eaf0023588
AnjoomNur <anjoom.nur@northsouth.edu>
2021-11-30 21:35:48 +0600

    contactUs Functionalities added

--- railapp/urls.py
+++ railapp/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('contact/', views.Contact, name="contact"),
+    path('contact/', views.ContactUs, name="contact"),
     path('checkout/', views.checkout, name="checkout"),
     path('confirmation/', views.Confirmation, name="confirmation"),
     path('faq/', views.faq, name="faq"),
--- railapp/views.py
+++ railapp/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.http import HttpResponse
 from .models import *
 # Create your views here.
 def home(request):
     return render(request, 'railapp/home.html')
-def Contact(request):
+def ContactUs(request):
+    if request.method=="POST":
+        contact=Contact()
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        contact.name=name
+        contact.email=email
+        contact.subject=subject
+        contact.save()
+        return redirect ('confirmation')
     return render(request, 'railapp/contact.html')
 def checkout(request):
     return render(request, 'railapp/checkout.html')
