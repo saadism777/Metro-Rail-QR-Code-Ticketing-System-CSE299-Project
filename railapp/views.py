494f6670dc3b186ae11864a095f17888de7531f2
Farhan Hasin Saad <farhan.hasin09@northsouth.edu>
2021-12-17 23:27:55 +0600

    Announcement and Travel History Implemented

--- railapp/views.py
+++ railapp/views.py
@@ -3,9 +3,29 @@ from django.core.mail import send_mail
 from django.contrib import messages
 from django.http import HttpResponse
 from .models import *
+from railapp import models
+from accounts.models import Book
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 def home(request):
-    return render(request, 'railapp/home.html')
+    if request.method=="POST":
+        print("This is post")
+        title = request.POST['title']
+        content = request.POST['content']
+        username = request.user.username
+        print(username,title,content)
+        ins = Announcement(username=username,title=title,content=content)
+        ins.save()
+        print("written successfully")
+    try:
+        post_list=Announcement.objects.order_by('created_on').reverse()
+        post_list_2=Announcement.objects.latest('created_on')
+        context = {'post_list':post_list, 'post_list_2':post_list_2}
+    except ObjectDoesNotExist:
+        context={}
+    
+    return render(request, 'railapp/home.html', context)
+
 def contact(request):
     if request.method=="POST":
         contact=Contact()
@@ -38,11 +58,18 @@ def faq(request):
         ques_obj = Questions.objects.all()
         ans_obj = Answers.objects.all()
         content={'ques_obj':ques_obj, 'ans_obj':ans_obj}
-        return render(request, 'railapp/faq.html', content)
+    return render(request, 'railapp/faq.html', content)
 
 
     
-def userprofile(request):
-    return render(request, 'railapp/userprofile.html')
+def userprofile(request,new={}):
+    context = {}
+    username_r = request.user.username
+    book_list = Book.objects.filter(username=username_r)
+    if book_list:
+        return render(request, 'railapp/userprofile.html', locals())
+    else:
+        context["error"] = "Sorry no buses booked"
+        return render(request, 'railapp/userprofile.html', context)
 def trainmasterprofile(request):
     return render(request, 'railapp/trainmasterprofile.html')
