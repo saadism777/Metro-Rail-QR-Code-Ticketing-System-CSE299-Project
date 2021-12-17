15ce0b79e3b500016527365e9928d25eaca7311d
Farhan Hasin Saad <farhan.hasin09@northsouth.edu>
2021-12-17 19:07:37 +0600

    Revert "Travel history implemented"
    This reverts commit 307271ef1699d891e1d463f9fbf5817404dfb6a2.

--- railapp/views.py
+++ railapp/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 from django.contrib import messages
 from django.http import HttpResponse
-<<<<<<< Updated upstream
 from railapp import models
-=======
-
->>>>>>> Stashed changes
 from accounts.models import Book
 from .models import *
 # Create your views here.
@@ -68,11 +64,7 @@ def userprofile(request,new={}):
         return render(request, 'railapp/userprofile.html', locals())
     else:
         context["error"] = "Sorry no buses booked"
-<<<<<<< Updated upstream
         return render(request, 'railapp/userprofile.html', context)
-=======
-        return render(request, 'userprofile.html', context)
->>>>>>> Stashed changes
 
 def trainmasterprofile(request):
     return render(request, 'railapp/trainmasterprofile.html')
